73de63d1b609bda6e46cc31f1c7eee17a4ca5c05
matias-bello-rodriguez <matias61100@gmail.com>
2025-09-10 14:47:22 -0300

    mover formulario

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.stopcae.cl', // Cambia esto por tu dominio real
   build: {
     assets: '_astro/assets'
   },
@@ -10,5 +13,6 @@ export default defineConfig({
     build: {
       assetsInlineLimit: 0
     }
-  }
+  },
+  integrations: [sitemap()],
 });
